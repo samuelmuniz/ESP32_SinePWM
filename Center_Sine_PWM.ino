a72b63186c7ebbcef617c0f4f2c1ed9749157c0d
unknown <samuelmunizsilva@gmail.com>
2022-07-21 13:44:16 +0200

    Adding static sine function

--- Center_Sine_PWM.ino
+++ Center_Sine_PWM.ino
@@ -1,16 +1,69 @@
 #include "math.h"
 
 const int  divi = 50;
+const float sinus[] = {0.13,
+0.25,
+0.37,
+0.48,
+0.59,
+0.68,
+0.77,
+0.84,
+0.90,
+0.95,
+0.98,
+1.00,
+1.00,
+0.98,
+0.95,
+0.90,
+0.84,
+0.77,
+0.68,
+0.59,
+0.48,
+0.37,
+0.25,
+0.13,
+0.00,
+-0.13,
+-0.25,
+-0.37,
+-0.48,
+-0.59,
+-0.68,
+-0.77,
+-0.84,
+-0.90,
+-0.95,
+-0.98,
+-1.00,
+-1.00,
+-0.98,
+-0.95,
+-0.90,
+-0.84,
+-0.77,
+-0.68,
+-0.59,
+-0.48,
+-0.37,
+-0.25,
+-0.13,
+0.00};
 
 
-unsigned long timerClock = 0;
+//unsigned long micros()= 0;
 unsigned long  timerClockPast = 0;
-int dutyCycle  = 0;
-int freq = 0;
+unsigned long  timerClockPastHz = 0;
+int dutyCycle1  = 0;
+int freq = 1;
 int ConvADC = 0;
 int aux = 0;
 
 void setup() {
+
+
   Serial.begin(115200);
   pinMode(22,OUTPUT);
   pinMode(23,OUTPUT);
@@ -19,8 +72,6 @@ void setup() {
   digitalWrite(23,HIGH);
 
  
-  
-
   // put your setup code here, to run once:
 
 }
@@ -28,28 +79,31 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  timerClock = micros();
 
 
   if(aux <= divi*freq){
-      if(timerClock - timerClockPast >= 200 ){
+      if(micros()- timerClockPast >= 200 ){
         
-        timerClockPast = timerClock;
-        dutyCycle = 50*sin(2*M_PI*(1000*aux/(divi*freq))*0.001) + 50;
+        timerClockPast = micros();
+        //dutyCycle1 = 50*sin(2*M_PI*(1000*aux/(divi*freq))*0.001) + 50;
+        dutyCycle1 = 50*sinus[aux/freq] + 50;
+
         aux++;
-        
+
       }
-      else if(timerClock - timerClockPast <= (100 - dutyCycle)){
+      else if(micros()- timerClockPast <= (100 - dutyCycle1)){
         
         digitalWrite(22,LOW);
         digitalWrite(23,HIGH); 
         
       }
-      else if((timerClock - timerClockPast >= (100 - dutyCycle)) && (timerClock - timerClockPast <= (100 + dutyCycle)) ){
+      else if((micros()- timerClockPast >= (100 - dutyCycle1)) && (micros()- timerClockPast <= (100 + dutyCycle1)) ){
+
         digitalWrite(23,LOW);
-        digitalWrite(22,HIGH);      
+        digitalWrite(22,HIGH); 
+
       }
-      else if((timerClock - timerClockPast > (100 + dutyCycle)) && (timerClock - timerClockPast < 200)){
+      else if((micros()- timerClockPast > (100 + dutyCycle1)) && (micros()- timerClockPast < 200)){
         
         digitalWrite(22,LOW);
         digitalWrite(23,HIGH); 
@@ -62,6 +116,9 @@ void loop() {
     aux = 0;
     ConvADC = analogRead(15);
     freq = map(ConvADC,0,1023,1,50);
+    timerClockPastHz = micros();
+    
+
     
     }
 
